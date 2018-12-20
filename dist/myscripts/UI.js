d608e296b336fa304b99309f938d7231edb7ede3
huseyin28 <huseyin2219@gmail.com>
2018-12-20 16:49:14 +0300

    asd

--- dist/myscripts/UI.js
+++ dist/myscripts/UI.js
@@ -1,6 +1,20 @@
 export class UI {
     static ToggleDashboardPanel(t){
-        $('#DashboardPanel').slideToggle("fast");
-        $(t).css({"background-color":"rgb(107,107,107)"});
+        $('#DashboardPanel').slideToggle("fast",()=>{
+            if($('#DashboardPanel').css("display") == "none") {
+                $(t).css({"background-color":"rgb(44, 44, 44)"});
+            }else{
+                $(t).css({"background-color":"rgb(107,107,107)"});
+            }
+        });
+    }
+    static ToggleEditablePanel(t){
+        $('#EditablePanel').slideToggle("fast",()=>{
+            if($('#EditablePanel').css("display") == "none") {
+                $(t).css({"background-color":"rgb(44, 44, 44)"});
+            }else{
+                $(t).css({"background-color":"rgb(107,107,107)"});
+            }
+        });
     }
 }
--- dist/myscripts/main.js
+++ dist/myscripts/main.js
@@ -5,7 +5,6 @@ $(document).ready(ready);
 
 function ready() {
     MapObject.init()
-    console.log(new Level1())
 }
 
 let MapObject = {
@@ -27,9 +26,10 @@ let MapObject = {
         L.control.layers(this.TileLayers).addTo(map);
 
 
-        this.addControl();
+        this.addEditableControl();
+        this.addDashboardControl();
     },
-    addControl: function () {
+    addDashboardControl: function () {
         var customControl = L.Control.extend({
             options: {
                 position: 'topright'
@@ -48,13 +48,6 @@ let MapObject = {
                 a.title = 'Dashboard';
                 container.appendChild(a);
 
-                container.onmouseover = function(){
-                    container.style.backgroundColor = '#efefef'; 
-                }
-                container.onmouseout = function(){
-                    container.style.backgroundColor = 'white'; 
-                }
-
                 a.onclick = function () {
                     UI.ToggleDashboardPanel(this);
                 }
@@ -63,9 +56,33 @@ let MapObject = {
             }
         });
         map.addControl(new customControl())
+    },
+    addEditableControl : function () {
+        var customControl = L.Control.extend({
+            options: {
+                position: 'topright'
+            },
+
+            onAdd: function (map) {
+                let container = L.DomUtil.create('div', 'leaflet-control-layers leaflet-control leaflet-control-custom');
+                let a = document.createElement('a');
+                a.className = 'leaflet-control-layers-toggle';
+                a.style.backgroundImage = 'none';
+                a.innerHTML = '<svg height="30px" viewBox="0 0 476.76426 476" width="30px"><path d="m451.023438 26.117188c-34.386719-34.3125-90.058594-34.3125-124.449219 0v.046874l-285.582031 285.550782c-.648438.683594-1.171876 1.472656-1.542969 2.335937-.101563.214844-.195313.433594-.273438.65625-.097656.21875-.1875.4375-.261719.664063l-38.65625 151.761718c-.714843 2.742188.082032 5.660157 2.085938 7.664063s4.921875 2.796875 7.664062 2.085937l151.753907-38.640624c.230469-.0625.429687-.183594.65625-.261719.230469-.078125.457031-.171875.679687-.273438.859375-.371093 1.648438-.894531 2.328125-1.542969l285.542969-285.558593.054688-.042969c34.320312-34.382812 34.320312-90.0625 0-124.445312zm-11.3125 11.308593c25.886718 25.949219 28.1875 67.183594 5.351562 95.851563l-101.191406-101.203125c28.664062-22.835938 69.898437-20.53125 95.839844 5.351562zm-107.472657 5.707031 101.808594 101.757813-28.679687 28.6875-101.808594-101.804687zm-303.445312 376.800782c12.628906 5.671875 22.742187 15.78125 28.414062 28.414062l-38.117187 9.703125zm44 24.421875c-7.367188-18.199219-21.800781-32.632813-40-40l18.144531-71.382813 93.230469 93.238282zm86.976562-25.199219-101.808593-101.785156 234.289062-234.289063 101.804688 101.808594zm0 0" fill="#8a8a8a"/></svg>';
+                a.style.backgroundPosition = 'center center';
+                a.style.paddingTop = '7px';
+                a.style.cursor = 'pointer';
+                a.style.textAlign = 'center';
+                a.title = 'Editable';
+                container.appendChild(a);
+
+                a.onclick = function () {
+                    UI.ToggleEditablePanel(this);
+                }
+
+                return container;
+            }
+        });
+        map.addControl(new customControl())
     }
 }
-
-/*
-<style type="text/css">div.image {max-width: 256px;max-height: 256px;background-image: url()}</style>
-*/
